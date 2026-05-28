5fd8bb6e605125f4588876807b7054cb9becb104
elie <eliebechehin77@gmail.com>
2026-05-28 10:57:58 +0000

    Methode add Ajouté

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,6 +1,7 @@
 
 package bookstoread;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -14,4 +15,17 @@ public class BookShelfSpec {
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
+
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java");
+        shelf.add("Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
 }
+
+
+
+
